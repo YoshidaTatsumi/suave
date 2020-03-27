fb73f7eebd5c3b40a0d005d89179e7c782a78732
YoshidaTatsumi <yoichi_1308@yahoo.co.jp>
2020-03-27 14:19:42 +0900

    [Fix]admin検索機能

--- app/controllers/admins/search_controller.rb
+++ app/controllers/admins/search_controller.rb
@@ -3,22 +3,11 @@ class Admins::SearchController < ApplicationController
 	
 	def search
 		if params[:category] == "user"
-			if params[:search_method] == "perfect_match"
-				@searches = User.where(name: "#{params[:search]}").page(params[:page]).per(10)
-			elsif params[:search_method] == "partial_match"
-				@searches = User.where("name LIKE?","%#{params[:search]}%").page(params[:page]).per(10)
-			end
-
+			@searches = User.where("name LIKE?","%#{params[:search]}%").page(params[:page]).per(10)
 		elsif params[:category] == "game"
-			if params[:search_method] == "perfect_match"
-				@searches = Game.where(title: "#{params[:search]}").page(params[:page]).per(10)
-			elsif params[:search_method] == "partial_match"
-				@searches = Game.where("title LIKE?","%#{params[:search]}%").page(params[:page]).per(10)
-			end
-
+			@searches = Game.where("title LIKE?","%#{params[:search]}%").page(params[:page]).per(10)
 		elsif params[:tag].present?
 			@searches = Game.tagged_with("#{params[:tag]}").page(params[:page]).per(10)
-
 		else
 			redirect_to admins_top_path
 		end
