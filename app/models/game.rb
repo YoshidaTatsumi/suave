2740592082bbc6c5f1c45cce3a797581df79fb70
YoshidaTatsumi <yoichi_1308@yahoo.co.jp>
2020-04-03 23:25:02 +0900

    [Fix]コメントされた人にだけ通知がいくように修正

--- app/models/game.rb
+++ app/models/game.rb
@@ -27,13 +27,8 @@ class Game < ApplicationRecord
 
 
 	def create_notification_comment!(current_user, comment_id)
-		# 自分以外にコメントしている人をすべて取得し、全員に通知を送る
-		temp_ids = Comment.select(:user_id).where(game_id: id).where.not(user_id: current_user.id).distinct
-	    temp_ids.each do |temp_id|
-	      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
-	    end
-	    # まだ誰もコメントしていない場合は、投稿者に通知を送る
-	    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+		comment = Comment.find(comment_id)
+	    save_notification_comment!(current_user, comment_id, user.id)
     end
 
     def save_notification_comment!(current_user, comment_id, visited_id)
@@ -44,7 +39,7 @@ class Game < ApplicationRecord
 			visited_id: visited_id,
 			action: 'comment'
 	    )
-	    # 自分の投稿に対するコメントの場合は、通知済みとする
+	    # 自分で自分の投稿にコメントした場合は、通知済みとする
 	    if notification.visitor_id == notification.visited_id
 			notification.checked = true
 	    end
