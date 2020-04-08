d89bc3f096fc3423622f9384b8393a74fc7fd5fd
YoshidaTatsumi <yoichi_1308@yahoo.co.jp>
2020-04-09 01:43:45 +0900

    [fix]ユーザー側チャットルーム一覧の順序変更

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,7 +2,7 @@ class ChatsController < ApplicationController
 	before_action :authenticate_user!, except: [:index]
 
 	def index
-		@rooms = Room.where.not(name: nil).page(params[:page]).per(8).reverse_order
+		@rooms = Room.where.not(name: nil).order(updated_at: :asc).page(params[:page]).per(8).reverse_order
 		@room = Room.new
 	end
 
